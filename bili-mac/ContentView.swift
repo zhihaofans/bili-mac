9385d292472321d45671bcea9a236ba3a9e793c5
zhihaofans <zhihaofans@hotmail.com>
2025-12-10 23:57:26 +0800

    Initial Commit

---
+++ bili-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bili-mac
+//
+//  Created by zzh on 2025/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bili-mac/bili_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  bili_macApp.swift
+//  bili-mac
+//
+//  Created by zzh on 2025/12/10.
+//
+
+import SwiftUI
+
+@main
+struct bili_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
