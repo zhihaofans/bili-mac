281016189b71df3087114c74b1520c19188fadb8
zhihaofans <zhihaofans@hotmail.com>
2025-12-17 23:21:36 +0800

    关闭最后一个窗口时应用会退出

--- bili-mac/bili_macApp.swift
+++ bili-mac/bili_macApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct bili_macApp: App {
+    // 这个加了之后，关闭最后一个窗口时，应用会退出
+    @NSApplicationDelegateAdaptor(AppDelegate.self)
+    var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(
+        _ sender: NSApplication
+    ) -> Bool {
+        true
     }
 }
